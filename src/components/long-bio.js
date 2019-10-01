e0f7e268872c1afbfa0ad034630b981b6d143aab
damiancanettirios <damian@grantdocs.com>
2019-10-01 00:03:12 -0500

    revised You in About

--- src/components/long-bio.js
+++ src/components/long-bio.js
@@ -11,60 +11,58 @@ const LongBio = ({ longBio }) => {
 
   return (
     <>
-      {longBio != null ? (
+      <div
+        style={{
+          marginTop: 30,
+          background: `#ecf0f1`,
+          width: `100%`,
+        }}
+      >
         <div
           style={{
-            marginTop: 30,
-            background: `#ecf0f1`,
-            width: `100%`,
+            display: `flex`,
+            flexDirection: `row`,
+            justifyContent: `space-between`,
+            paddingTop: 5,
+            paddingBottom: 5,
           }}
         >
-          <div
+          <p
             style={{
-              display: `flex`,
-              flexDirection: `row`,
-              justifyContent: `space-between`,
-              paddingTop: 5,
+              fontSize: `1.1rem`,
+              textAlign: `left`,
+              paddingLeft: 5,
+            }}
+          >
+            {longBio != null ? "BACKGROUND" : "ABOUT YOU"}
+          </p>
+          <IconButton onClick={() => setActive(!active)}>
+            {!active ? (
+              <FontAwesomeIcon icon={faChevronDown} />
+            ) : (
+              <FontAwesomeIcon icon={faChevronUp} />
+            )}
+          </IconButton>
+        </div>
+        {!active ? (
+          <section />
+        ) : (
+          <p
+            style={{
+              fontSize: `.85rem`,
+              textAlign: `left`,
+              marginTop: 20,
+              paddingLeft: 5,
+              paddingRight: 5,
               paddingBottom: 5,
             }}
           >
-            <p
-              style={{
-                fontSize: `1.1rem`,
-                textAlign: `left`,
-                paddingLeft: 5,
-              }}
-            >
-              BACKGROUND
-            </p>
-            <IconButton onClick={() => setActive(!active)}>
-              {!active ? (
-                <FontAwesomeIcon icon={faChevronDown} />
-              ) : (
-                <FontAwesomeIcon icon={faChevronUp} />
-              )}
-            </IconButton>
-          </div>
-          {!active ? (
-            <section />
-          ) : (
-            <p
-              style={{
-                fontSize: `.85rem`,
-                textAlign: `left`,
-                marginTop: 20,
-                paddingLeft: 5,
-                paddingRight: 5,
-                paddingBottom: 5,
-              }}
-            >
-              {longBio.longBio}
-            </p>
-          )}
-        </div>
-      ) : (
-        <div style={{ paddingBottom: `3.5rem` }} />
-      )}
+            {longBio != null
+              ? longBio.longBio
+              : "We are looking for intellectually curious people who want to help accelerate the impact entrepreneurs and innovative companies have in Australia. If this sounds interesting to you, click the @ sign above and send us a note telling us why. Open to anyone - students, experienced hires, remote workers, etc."}
+          </p>
+        )}
+      </div>
     </>
   )
 }
